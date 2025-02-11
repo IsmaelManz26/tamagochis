2883f0755b38f42a55b573ac22b2f64e7c1b0fe0
Ismael Manzano Reinoso <imanrei343@ieszaidinvergeles.org>
2025-02-11 20:07:03 +0100

    Intento erroneo de mensaje

--- clienteConTableroFuncionando/src/entities/Board.js
+++ clienteConTableroFuncionando/src/entities/Board.js
@@ -1,35 +1,38 @@
 export const ELEMENTS = {
-    bush : 'bush',
-    player: 'player'
+  bush: "bush",
+  player: "player",
 };
 export class Board {
-    #map = null;
-    #states = {
-        NO_BUILD : 0,
-        BUILD : 1
-    }
-    #state = null;
+  #map = null;
+  #states = {
+    NO_BUILD: 0,
+    BUILD: 1,
+  };
+  #state = null;
 
-    constructor() {
-        this.#state = this.#states.NO_BUILD;
-    }
+  constructor() {
+    this.#state = this.#states.NO_BUILD;
+  }
 
-    build(payload) {
-        const { size, elements } = payload;
-        this.#map = new Array(size).fill().map(() => new Array(size).fill(0));
-        elements.forEach(element => {
-            if (element.type === 'bush') {
-                this.#map[element.x][element.y] = ELEMENTS.bush;
-            } else if (element.type === 'player') {
-                this.#map[element.x][element.y] = ELEMENTS.player;
-            }
-        });
-        this.#state = this.#states.BUILD;
-    }
+  // desestructuracion del objeto payload que esta compuesto de size y elements
+  build(payload) {
+    const { size, elements } = payload;
+    // Creo una matriz bidimensional this.#map inicializada con ceros
+    this.#map = new Array(size).fill().map(() => new Array(size).fill(0));
+    elements.forEach((element) => {
+      if (element.type === "bush") {
+        this.#map[element.x][element.y] = ELEMENTS.bush;
+      } else if (element.type === "player") {
+        this.#map[element.x][element.y] = ELEMENTS.player;
+      }
+    });
+    this.#state = this.#states.BUILD;
+  }
 
-    get map() {
-        if (this.#state === this.#states.BUILD) {
-            return this.#map;
-        } return undefined;
+  get map() {
+    if (this.#state === this.#states.BUILD) {
+      return this.#map;
     }
+    return undefined;
+  }
 }
--- server/src/game/BoardBuilder.ts
+++ server/src/game/BoardBuilder.ts
@@ -1,55 +1,67 @@
 import { Board } from "./entities/Board.js";
 
 export class BoardBuilder {
-    private board: Board;
-    
-    constructor(payload: { size: number, numBushes: number }) {
-        this.board = {
-            size: payload.size,
-            elements: []
-        }
+  private board: Board;
 
-        // this.board = {
-        //     size: 8,
-        //     elements: []
-        // }
-        // const map : Array<number[]> = [
-        //     [0,0,0,0,0,0,0,0],
-        //     [0,0,0,0,0,0,5,0],
-        //     [0,5,0,0,0,0,0,0],
-        //     [0,0,0,0,0,0,0,0],
-        //     [0,0,0,0,0,5,0,0],
-        //     [0,0,0,0,0,0,0,0],
-        //     [0,0,0,0,0,0,5,0],
-        //     [0,0,5,0,0,0,0,0]
-        // ]
-        // for(let i = 0; i < this.board.size; i++)
-        //     for(let j = 0; j < this.board.size; j++)
-        //         if(map[i][j] != 0) {
-        //             this.board.elements.push({x : i, y : j})
-        //         }
-        
-        this.board.elements.push({x: 0, y: 0, type: 'player'});
-        this.board.elements.push({x: 0, y: this.board.size - 1, type: 'player'});
-        this.board.elements.push({x: this.board.size - 1, y: 0, type: 'player'});
-        this.board.elements.push({x: this.board.size - 1, y: this.board.size - 1, type: 'player'});
+  constructor(payload: { size: number; numBushes: number }) {
+    this.board = {
+      size: payload.size,
+      elements: [],
+    };
 
-        // Añadir arbustos en posiciones aleatorias
-        const numBushes = payload.numBushes;
-        let bushesAdded = 0;
-        while (bushesAdded < numBushes) {
-            const x = Math.floor(Math.random() * this.board.size);
-            const y = Math.floor(Math.random() * this.board.size);
-            // Evitar colocar arbustos en las posiciones de los jugadores y otros arbustos
-            if (!this.board.elements.some(e => e.x === x && e.y === y)) {
-                this.board.elements.push({x, y, type: 'bush'});
-                bushesAdded++;
-            }
-        }
+    // this.board = {
+    //     size: 8,
+    //     elements: []
+    // }
+    // const map : Array<number[]> = [
+    //     [0,0,0,0,0,0,0,0],
+    //     [0,0,0,0,0,0,5,0],
+    //     [0,5,0,0,0,0,0,0],
+    //     [0,0,0,0,0,0,0,0],
+    //     [0,0,0,0,0,5,0,0],
+    //     [0,0,0,0,0,0,0,0],
+    //     [0,0,0,0,0,0,5,0],
+    //     [0,0,5,0,0,0,0,0]
+    // ]
+    // for(let i = 0; i < this.board.size; i++)
+    //     for(let j = 0; j < this.board.size; j++)
+    //         if(map[i][j] != 0) {
+    //             this.board.elements.push({x : i, y : j})
+    //         }
 
+    this.board.elements.push({ x: 0, y: 0, type: "player-1" });
+    this.board.elements.push({
+      x: 0,
+      y: this.board.size - 1,
+      type: "player-2",
+    });
+    this.board.elements.push({
+      x: this.board.size - 1,
+      y: 0,
+      type: "player-3",
+    });
+    this.board.elements.push({
+      x: this.board.size - 1,
+      y: this.board.size - 1,
+      type: "player-4",
+    });
+
+    // Añadir arbustos en posiciones aleatorias
+    const numBushes = payload.numBushes;
+    let bushesAdded = 0;
+    while (bushesAdded < numBushes) {
+      const x = Math.floor(Math.random() * this.board.size);
+      const y = Math.floor(Math.random() * this.board.size);
+      // Evitar colocar arbustos en las posiciones de los jugadores y otros arbustos
+      // si la posicion esta libre 'some' devuelve false y agrega de nuevo el objeto arbusto
+      if (!this.board.elements.some((e) => e.x === x && e.y === y)) {
+        this.board.elements.push({ x, y, type: "bush" });
+        bushesAdded++;
+      }
     }
+  }
 
-    public getBoard() : Board {
-        return this.board;
-    }
+  public getBoard(): Board {
+    return this.board;
+  }
 }
