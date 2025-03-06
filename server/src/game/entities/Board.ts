98ee273d1fd574d1a97e405044479050e83b4ee1
Ismael Manzano Reinoso <imanrei343@ieszaidinvergeles.org>
2025-03-06 17:32:01 +0100

    Type Element player or bush

--- server/src/game/entities/Board.ts
+++ server/src/game/entities/Board.ts
@@ -1,11 +1,15 @@
 import { Player } from "../../player/entities/Player";
+
+export type ElementType = "player" | "bush";
+
 export interface Element {
-    x : number;
-    y : number;
-    type ?: string;
+  x: number;
+  y: number;
+  type: ElementType;
+  identificador?: string;
 }
 
 export interface Board {
-    size: number;
-    elements: Element[];
+  size: number;
+  elements: Element[];
 }
